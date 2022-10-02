5a4d738d7cab4754212c2319ebffd941088cbc13
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-02 17:16:23 -0500

    Add annotation to compile model

--- main.py
+++ main.py
@@ -133,19 +133,23 @@ class CycleGAN(keras.Model):
         self.kl_loss_tracker = tf.keras.metrics.Mean(name="kl_loss")
 
     # Loss Functions
+    @tf.function
     def discriminator_loss(self, real, generated):
       real_loss = self.loss_obj(tf.ones_like(real), real)
       generated_loss = self.loss_obj(tf.zeros_like(generated), generated)
       total_disc_loss = real_loss + generated_loss
       return total_disc_loss * 0.5
 
+    @tf.function
     def generator_loss(self, generated):
       return self.loss_obj(tf.ones_like(generated), generated)
 
+    @tf.function
     def calc_cycle_loss(self, real_image, cycled_image):
       loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
       return LAMBDA * loss1
 
+    @tf.function
     def identity_loss(self, real_image, same_image):
       loss = tf.reduce_mean(tf.abs(real_image - same_image))
       return LAMBDA * 0.5 * loss
